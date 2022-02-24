9fe8db9bbd17464ac1069e31efe5ceffc894e9bf
jf98uni <jf.botero@uniandes.edu.co>
2022-02-23 19:06:13 -0500

    Tercer video, una aplicacion funcional que se conecta con la api escrita en java

--- kata-crud/src/App.js
+++ kata-crud/src/App.js
@@ -1,10 +1,69 @@
-import React,{useContext} from 'react';
+import React,{createContext, useContext, useReducer, useEffect,useRef,useState} from 'react';
 
 const HOST_API = "http://localhost:8080/api"
+
+const initialState = {
+  list: []
+}
+
+const Store = createContext(initialState)
+
+const Form = () => {
+
+  const formRef = useRef(null);
+  const {dispatch} = useContext(Store)
+  const [state, setState] = useState({})
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    // body o json para los datos 
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted: false
+    }
+
+    fetch(HOST_API+"/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response =>response.json())
+    .then ((todo) => {
+      dispatch({type: "add-item", item: todo});
+      setState({name: ""});
+      formRef.current.reset();
+    })
+    
+  }
+
+
+
+  return <form ref={formRef}>
+    <input type="text" name="name" onChange={(event)=> {
+      setState({...state, name: event.target.value})
+    }}></input>
+    <button onClick={onAdd}>Agregar</button>
+  </form>
+
+
+}
 
 const List = () => {
 
   const {dispatch,state} = useContext(Store)
+
+  useEffect(()=>{
+    fetch(HOST_API+"/todos")
+    .then(response => response.json())
+    .then((list)=> {
+      dispatch({type: "update-list", list})
+    })
+
+  },[state.list.length, dispatch])
 
   return <div>
     <table>
@@ -29,11 +88,37 @@ const List = () => {
   
 }
 
+
+function reducer(state, action) {
+
+  switch (action.type) {
+    case 'update-list':
+      return{...state, list: action.list}
+    case 'add-item':
+      const newList = state.list;
+      newList.push(action.item);
+      return {...state, list: newList}
+  
+    default:
+      return state;
+  }
+  
+}
+
+const StoreProvider = ({ children}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value={{state, dispatch}}>
+    {children}
+  </Store.Provider>
+}
+
 function App() {
   return (
-    <div>
-    
-    </div>
+    <StoreProvider>
+      <Form></Form>
+      <List></List>
+    </StoreProvider>
   );
 }
 
